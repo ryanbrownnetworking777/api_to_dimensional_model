b266bf7bae7d31048e0952c9422494f7ae56257e
Ryan Brown <ryanbrownnetworking777@gmail.com>
2023-05-02 15:43:23 -0500

    Moving saving functions out of inner processing of the dimensions

--- src/api_to_dimensional_model.py
+++ src/api_to_dimensional_model.py
@@ -28,36 +28,26 @@ def isolate_dimension(df, dimension_columns,  new_dimension_columns):
     dimension_df.columns = new_dimension_columns
     return dimension_df
 
-def initialize_dimension(dimension_df, dimension_check_function, dimension_check_function_arguments, dimension_name, saving_function, saving_function_arguments={}) -> None:
-    if dimension_check_function(**dimension_check_function_arguments):
-        print(f"{dimension_name} dimension is already initialized.")
-        return 
-    else:
-        dimension_df = dimension_df.drop_duplicates().reset_index(drop='index')
-        dimension_id_string = f'{dimension_name}_id'
-        dimension_columns = [dimension_id_string] + list(dimension_df.columns)
-        dimension_df[dimension_id_string] = dimension_df.index
-        dimension_df = dimension_df[dimension_columns]
-        today =  int(datetime.datetime.today().date().strftime("%Y%m%d"))
-        dimension_df['effective_from'] = today
-        dimension_df['effective_till'] = 99990101
-        dimension_df['is_active'] = 'Y'
-        dimension_df = dimension_df.fillna("N/A")
-        if saving_function_arguments != {}:
-            saving_function_arguments['df'] = dimension_df
-            saving_function(**saving_function_arguments)
-        else:
-            saving_function(df=dimension_df)
-        print(f"{dimension_name} dimension initialized.")
-        return 
+def initialize_dimension(dimension_df,  dimension_name) -> pd.DataFrame:
+    dimension_df = dimension_df.drop_duplicates().reset_index(drop='index')
+    dimension_id_string = f'{dimension_name}_id'
+    dimension_columns = [dimension_id_string] + list(dimension_df.columns)
+    dimension_df[dimension_id_string] = dimension_df.index
+    dimension_df = dimension_df[dimension_columns]
+    today =  int(datetime.datetime.today().date().strftime("%Y%m%d"))
+    dimension_df['effective_from'] = today
+    dimension_df['effective_till'] = 99990101
+    dimension_df['is_active'] = 'Y'
+    dimension_df = dimension_df.fillna("N/A")
+    print(f"{dimension_name} dimension initialized.")
+    return dimension_df 
 
-def append_dimension(dimension_df, dimension_name, dimension_columns, loading_function, loading_function_arguments, saving_function, saving_function_arguments={}) -> None:
+def append_dimension(dimension_df, existing_dimension_df, dimension_name, dimension_columns) -> pd.DataFrame:
 
-    dimension_disk_df = loading_function(**loading_function_arguments)
     today =  int(datetime.datetime.today().date().strftime("%Y%m%d"))
     new_dimension_df = pd.merge(
             dimension_df[dimension_columns]
-            ,dimension_disk_df[dimension_columns]
+            ,existing_dimension_df[dimension_columns]
             ,how='outer'
             ,indicator=True
             )
@@ -66,19 +56,14 @@ def append_dimension(dimension_df, dimension_name, dimension_columns, loading_fu
     new_dimension_df['effective_till'] = 99990101
     new_dimension_df['is_active'] = 'Y'
     dimension_id_string = f"{dimension_name}_id"
-    max_id = dimension_disk_df[dimension_id_string].max()
+    max_id = existing_dimension_df[dimension_id_string].max()
     new_dimension_df[dimension_id_string] = [int(val + max_id+1) for val in new_dimension_df.index]
-    dimension_df = pd.concat([dimension_disk_df, new_dimension_df]) 
+    dimension_df = pd.concat([existing_dimension_df, new_dimension_df]) 
     dimension_df[dimension_id_string] = dimension_df[dimension_id_string].astype(int)
-    if saving_function_arguments !={}:
-        saving_function_arguments['df'] = dimension_df
-        saving_function(**saving_function_arguments)
-    else:
-        saving_function(df=dimension_df)
     print(f"{dimension_name} dimension updated with {len(new_dimension_df)} new records.")
-    return
+    return dimension_df
     
-def deactivate_dimension_entries(dimension_df, dimension_name, entries_to_deactivate_df, saving_function, saving_function_arguments={}) -> None:
+def deactivate_dimension_entries(dimension_df, entries_to_deactivate_df )-> pd.DataFrame:
     today =  int(datetime.datetime.today().date().strftime("%Y%m%d"))
     to_deactivate_entries = pd.merge(
             dimension_df
@@ -87,47 +72,54 @@ def deactivate_dimension_entries(dimension_df, dimension_name, entries_to_deacti
             ,on = list(entries_to_deactivate_df.columns)
             ,indicator=True
             )
-    to_deactivate_entries_locations = list(to_deactivate_entries[(to_deactivate_entries._merge == '_both')].index)
+    to_deactivate_entries_locations = list(to_deactivate_entries[(to_deactivate_entries._merge == 'both')].index)
     print(to_deactivate_entries_locations)
-    # effective_till = list(dimension_df['effective_till'])
-    # effective_till[to_deactivate_entries_locations] = today
-    # is_active = list(dimension_df['is_active'])
-    # is_active[to_deactivate_entries_locations] = 'N'
-    # dimension_df['effective_till'] = effective_till
-    # dimension_df['is_active'] = is_active
-    # if saving_function_arguments !={}:
-    #     saving_function_arguments['df'] = dimension_df
-    #     saving_function(**saving_function_arguments)
-    # else:
-    #     saving_function(df=dimension_df)
+    effective_till = (dimension_df['effective_till']).copy()
+    effective_till[to_deactivate_entries_locations] = today
+    is_active = (dimension_df['is_active']).copy()
+    is_active[to_deactivate_entries_locations] = 'N'
+    dimension_df['effective_till'] = effective_till
+    dimension_df['is_active'] = is_active
     # print(f"{len(to_deactivate_entries_locations)} deactivated from dimension {dimension_name}")
-    return
+    return dimension_df
 
 
 
-def process_dimension(df, dimension_columns, dimension_name, conversion_function, conversion_function_arguments, dimension_check_function, dimension_check_function_arguments, saving_function, saving_function_arguments) -> None: 
+def process_dimension(df, dimension_columns, dimension_name, conversion_function, conversion_function_arguments, dimension_check_function, dimension_check_function_arguments, saving_function, saving_function_arguments, loading_function, loading_function_arguments) -> None: 
     new_dimension_columns = create_dimension_columns(
                                                     columns=dimension_columns
                                                     ,conversion_function=conversion_function
                                                     ,conversion_function_arguments=conversion_function_arguments
                                                     )
-    dimension_df = isolate_dimension(df=df, 
+    dimension_df = isolate_dimension(
+                                     df=df, 
                                      dimension_columns=dimension_columns, 
-                                     new_dimension_columns=new_dimension_columns)
-    # return dimension_df
-    initialize_dimension(dimension_df=dimension_df 
+                                     new_dimension_columns=new_dimension_columns
+                                     )
+    dimension_check = dimension_check_function(**dimension_check_function_arguments) 
+    if  dimension_check == False:
+        dimension_df = initialize_dimension(
+                                        dimension_df=dimension_df 
                                         ,dimension_name = dimension_name
-                                        ,dimension_check_function=dimension_check_function
-                                        ,dimension_check_function_arguments=dimension_check_function_arguments
-                                        ,saving_function=saving_function
-                                        ,saving_function_arguments=saving_function_arguments
                                         )  
-                                       
-    append_dimension(dimension_df=dimension_df 
-                     ,dimension_path=dimension_path 
-                     ,dimension_columns=dimension_columns
-                     ,dimension_name=dimension_name
-                    )
+        if saving_function_arguments != {}:
+            saving_function_arguments['df'] = dimension_df
+            saving_function(**saving_function_arguments)
+        else:
+            saving_function(df=dimension_df)
+    elif dimension_check == True:
+        existing_dimension_df = loading_function(**loading_function_arguments)
+        dimension_df = append_dimension(
+                        dimension_df=dimension_df 
+                        ,existing_dimension_df=existing_dimension_df 
+                        ,dimension_columns=new_dimension_columns
+                        ,dimension_name = dimension_name
+                        )
+        if saving_function_arguments !={}:
+            saving_function_arguments['df'] = dimension_df
+            saving_function(**saving_function_arguments)
+        else:
+            saving_function(df=dimension_df)
     return 
 # def isolate_fact(df, fact_columns, new_fact_columns):
 #     fact_df = df[fact_columns]
@@ -142,13 +134,13 @@ A dictionary will specify the following:
         {
         
             "processing_function": processing_function
-            ,"processing_function_arguments": [arguments as list] 
+            ,"processing_function_arguments": {arguments as dict} 
         }
     
     }
 """
 def string_columns_to_integer_id(df, id_column_name, columns, loading_function, loading_function_arguments):
-    dimension_df = loading_function(loading_function_arguments)
+    dimension_df = loading_function(**loading_function_arguments)
     id_df = df[columns]
     id = pd.merge(
             id_df
@@ -163,7 +155,7 @@ def create_fact(df, fact_column_processing_dict):
     new_columns = [column_key for column_key in fact_column_processing_dict.keys()]
     for new_column in new_columns:
         column_dict = fact_column_processing_dict[new_column]
-        df[new_column] = column_dict['processing_function'](*column_dict['processing_function_arguments'])
+        df[new_column] = column_dict['processing_function'](**column_dict['processing_function_arguments'])
     new_df = df[new_columns]
     return new_df
 
