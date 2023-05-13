4d52d2b37828aae3766d5af6e70edcc8c119e156
Ryan Brown <ryanbrownnetworking777@gmail.com>
2023-05-13 16:44:42 -0500

    fixing the append_fact function

--- src/api_to_dimensional_model.py
+++ src/api_to_dimensional_model.py
@@ -161,13 +161,16 @@ def create_fact(df, fact_column_processing_dict):
     return new_df
 
 
-def append_fact(fact_df, fact_name, existing_fact_df) -> pd.DataFrame:
-    new_fact_df = fact_df.combine_first(existing_fact_df)
-    appended_len = len(new_fact_df) - len(existing_fact_df)
+def append_fact(fact_df, fact_name, existing_fact_df, date_column) -> pd.DataFrame:
+    max_date = existing_fact_df[date_column].max()
+    print(max_date)
+    new_fact_df = fact_df[fact_df[date_column]>=max_date]
+    appended_len = len(new_fact_df)
+    new_fact_df = pd.concat([existing_fact_df, new_fact_df]).drop_duplicates()
     print(f"{fact_name} fact appended with {appended_len} new records.")
     return new_fact_df
 
-def process_fact(df, fact_name, fact_column_processing_dict, table_check_function, table_check_function_arguments, saving_function, saving_function_arguments, loading_function, loading_function_arguments)  -> None: 
+def process_fact(df, fact_name, fact_column_processing_dict, table_check_function, table_check_function_arguments, saving_function, saving_function_arguments, loading_function, loading_function_arguments, date_column)  -> None: 
     fact_df = create_fact(df=df, fact_column_processing_dict=fact_column_processing_dict)
     fact_check = table_check_function(**table_check_function_arguments) 
     print(fact_check)
@@ -180,9 +183,10 @@ def process_fact(df, fact_name, fact_column_processing_dict, table_check_functio
         print(f"Fact table {fact_name} initalized.")
     elif fact_check == True:
         existing_fact_df = loading_function(**loading_function_arguments)
-        fact_df = append_fact(fact_df=fact_df, 
-                              existing_fact_df=existing_fact_df, 
-                              fact_name=fact_name)
+        fact_df = append_fact(fact_df=fact_df 
+                              ,existing_fact_df=existing_fact_df 
+                              ,fact_name=fact_name
+                              ,date_column = date_column)
         if saving_function_arguments !={}:
             saving_function_arguments['df'] = fact_df
             saving_function(**saving_function_arguments)
